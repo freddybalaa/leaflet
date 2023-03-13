7c3d2f8c7ae008d6fcc93970a098ab4c16aad3f1
freddybalaa <frederic.balaa@outlook.com>
2023-03-13 18:24:19 +0200

    1.0

---
+++ js/map.js
@@ -0,0 +1,9 @@
+var map = L.map('map').setView([34.0697,-118.4432], 17);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+var marker = L.marker([33.839894, 35.534677]).addTo(map)
+		.bindPopup('Freddy balaa  <br> Home')
+		.openPopup();	
